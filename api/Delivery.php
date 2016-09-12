63b14d8b0adfb49f2cd0c606bb0b789d1aba758c
Erko Evgen <osben90@gmail.com>
2016-09-12 13:17:41 +0300

    php-cs-fixer scan dir /Api

--- api/Delivery.php
+++ api/Delivery.php
@@ -14,87 +14,88 @@ require_once('Simpla.php');
 class Delivery extends Simpla
 {
 
-	public function get_delivery($id)
-	{
-		$query = $this->db->placehold("SELECT d.id, d.name, d.description, d.free_from, d.price, d.enabled, d.position, d.separate_payment
+    public function get_delivery($id)
+    {
+        $query = $this->db->placehold("SELECT d.id, d.name, d.description, d.free_from, d.price, d.enabled, d.position, d.separate_payment
 										FROM __delivery d
 										WHERE d.id=?
 										LIMIT 1", intval($id));
 
-		$this->db->query($query);
-		return $this->db->result();
-	}
+        $this->db->query($query);
+        return $this->db->result();
+    }
 
-	public function get_deliveries($filter = array())
-	{
-		// По умолчанию
-		$enabled_filter = '';
+    public function get_deliveries($filter = array())
+    {
+        // По умолчанию
+        $enabled_filter = '';
 
-		if(!empty($filter['enabled']))
-			$enabled_filter = $this->db->placehold('AND d.enabled=?', intval($filter['enabled']));
+        if (!empty($filter['enabled'])) {
+            $enabled_filter = $this->db->placehold('AND d.enabled=?', intval($filter['enabled']));
+        }
 
-		$query = $this->db->placehold("SELECT d.id, d.name, d.description, d.free_from, d.price, d.enabled, d.position, d.separate_payment
+        $query = $this->db->placehold("SELECT d.id, d.name, d.description, d.free_from, d.price, d.enabled, d.position, d.separate_payment
 										FROM __delivery d
 										WHERE 1
 											$enabled_filter
 										ORDER BY d.position");
 
-		$this->db->query($query);
+        $this->db->query($query);
 
-		return $this->db->results();
-	}
+        return $this->db->results();
+    }
 
-	public function update_delivery($id, $delivery)
-	{
-		$query = $this->db->placehold("UPDATE __delivery SET ?% WHERE id in(?@)", $delivery, (array)$id);
-		$this->db->query($query);
+    public function update_delivery($id, $delivery)
+    {
+        $query = $this->db->placehold("UPDATE __delivery SET ?% WHERE id in(?@)", $delivery, (array)$id);
+        $this->db->query($query);
 
-		return $id;
-	}
+        return $id;
+    }
 
-	public function add_delivery($delivery)
-	{
-		$query = $this->db->placehold('INSERT INTO __delivery SET ?%', $delivery);
+    public function add_delivery($delivery)
+    {
+        $query = $this->db->placehold('INSERT INTO __delivery SET ?%', $delivery);
 
-		if(!$this->db->query($query))
-			return false;
+        if (!$this->db->query($query)) {
+            return false;
+        }
 
-		$id = $this->db->insert_id();
-		$this->db->query("UPDATE __delivery SET position=id WHERE id=?", intval($id));
+        $id = $this->db->insert_id();
+        $this->db->query("UPDATE __delivery SET position=id WHERE id=?", intval($id));
 
-		return $id;
-	}
+        return $id;
+    }
 
-	public function delete_delivery($id)
-	{
-		if(!empty($id))
-		{
-			// Удаляем связь метода доставки с оплатами
-			$query = $this->db->placehold("DELETE FROM __delivery_payment WHERE delivery_id=?", intval($id));
-			$this->db->query($query);
+    public function delete_delivery($id)
+    {
+        if (!empty($id)) {
+            // Удаляем связь метода доставки с оплатами
+            $query = $this->db->placehold("DELETE FROM __delivery_payment WHERE delivery_id=?", intval($id));
+            $this->db->query($query);
 
-			$query = $this->db->placehold("DELETE FROM __delivery WHERE id=? LIMIT 1", intval($id));
-			$this->db->query($query);
-		}
-
-	}
+            $query = $this->db->placehold("DELETE FROM __delivery WHERE id=? LIMIT 1", intval($id));
+            $this->db->query($query);
+        }
+    }
 
 
-	public function get_delivery_payments($id)
-	{
-		$query = $this->db->placehold("SELECT payment_method_id FROM __delivery_payment WHERE delivery_id=?", intval($id));
-		$this->db->query($query);
+    public function get_delivery_payments($id)
+    {
+        $query = $this->db->placehold("SELECT payment_method_id FROM __delivery_payment WHERE delivery_id=?", intval($id));
+        $this->db->query($query);
 
-		return $this->db->results('payment_method_id');
-	}
+        return $this->db->results('payment_method_id');
+    }
 
-	public function update_delivery_payments($id, $payment_methods_ids)
-	{
-		$query = $this->db->placehold("DELETE FROM __delivery_payment WHERE delivery_id=?", intval($id));
-		$this->db->query($query);
-		if(is_array($payment_methods_ids))
-			foreach($payment_methods_ids as $p_id)
-				$this->db->query("INSERT INTO __delivery_payment SET delivery_id=?, payment_method_id=?", $id, $p_id);
-	}
-
+    public function update_delivery_payments($id, $payment_methods_ids)
+    {
+        $query = $this->db->placehold("DELETE FROM __delivery_payment WHERE delivery_id=?", intval($id));
+        $this->db->query($query);
+        if (is_array($payment_methods_ids)) {
+            foreach ($payment_methods_ids as $p_id) {
+                $this->db->query("INSERT INTO __delivery_payment SET delivery_id=?, payment_method_id=?", $id, $p_id);
+            }
+        }
+    }
 }
--- api/Simpla.php
+++ api/Simpla.php
@@ -14,96 +14,88 @@
  */
 class Simpla
 {
-	// Свойства - Классы API
-	private $classes = array(
-		'config'     => 'Config',
-		'request'    => 'Request',
-		'db'         => 'Database',
-		'settings'   => 'Settings',
-		'design'     => 'Design',
-		'products'   => 'Products',
-		'variants'   => 'Variants',
-		'categories' => 'Categories',
-		'brands'     => 'Brands',
-		'features'   => 'Features',
-		'money'      => 'Money',
-		'pages'      => 'Pages',
-		'blog'       => 'Blog',
-		'cart'       => 'Cart',
-		'image'      => 'Image',
-		'delivery'   => 'Delivery',
-		'payment'    => 'Payment',
-		'orders'     => 'Orders',
-		'users'      => 'Users',
-		'coupons'    => 'Coupons',
-		'comments'   => 'Comments',
-		'feedbacks'  => 'Feedbacks',
-		'notify'     => 'Notify',
-		'managers'   => 'Managers'
-	);
+    // Свойства - Классы API
+    private $classes = array(
+        'config'     => 'Config',
+        'request'    => 'Request',
+        'db'         => 'Database',
+        'settings'   => 'Settings',
+        'design'     => 'Design',
+        'products'   => 'Products',
+        'variants'   => 'Variants',
+        'categories' => 'Categories',
+        'brands'     => 'Brands',
+        'features'   => 'Features',
+        'money'      => 'Money',
+        'pages'      => 'Pages',
+        'blog'       => 'Blog',
+        'cart'       => 'Cart',
+        'image'      => 'Image',
+        'delivery'   => 'Delivery',
+        'payment'    => 'Payment',
+        'orders'     => 'Orders',
+        'users'      => 'Users',
+        'coupons'    => 'Coupons',
+        'comments'   => 'Comments',
+        'feedbacks'  => 'Feedbacks',
+        'notify'     => 'Notify',
+        'managers'   => 'Managers'
+    );
 
-	// Созданные объекты
-	private static $objects = array();
+    // Созданные объекты
+    private static $objects = array();
 
-	/**
-	 * Конструктор оставим пустым, но определим его на случай обращения parent::__construct() в классах API
-	 */
-	public function __construct()
-	{
-		//error_reporting(E_ALL & !E_STRICT);
-	}
+    /**
+     * Конструктор оставим пустым, но определим его на случай обращения parent::__construct() в классах API
+     */
+    public function __construct()
+    {
+        //error_reporting(E_ALL & !E_STRICT);
+    }
 
-	/**
-	 * Магический метод, создает нужный объект API
-	 */
-	public function __get($name)
-	{
-		// Если такой объект уже существует, возвращаем его
-		if(isset(self::$objects[$name]))
-		{
-			return(self::$objects[$name]);
-		}
+    /**
+     * Магический метод, создает нужный объект API
+     */
+    public function __get($name)
+    {
+        // Если такой объект уже существует, возвращаем его
+        if (isset(self::$objects[$name])) {
+            return(self::$objects[$name]);
+        }
 
-		// Если запрошенного API не существует - ошибка
-		if(!array_key_exists($name, $this->classes))
-		{
-			return null;
-		}
+        // Если запрошенного API не существует - ошибка
+        if (!array_key_exists($name, $this->classes)) {
+            return null;
+        }
 
-		// Определяем имя нужного класса
-		$class = $this->classes[$name];
+        // Определяем имя нужного класса
+        $class = $this->classes[$name];
 
-		// Подключаем его
-		include_once(dirname(__FILE__).'/'.$class.'.php');
+        // Подключаем его
+        include_once(dirname(__FILE__).'/'.$class.'.php');
 
-		// Сохраняем для будущих обращений к нему
-		self::$objects[$name] = new $class();
+        // Сохраняем для будущих обращений к нему
+        self::$objects[$name] = new $class();
 
-		// Возвращаем созданный объект
-		return self::$objects[$name];
-	}
+        // Возвращаем созданный объект
+        return self::$objects[$name];
+    }
 
-	/*
-		Вспомогательные методы
-	*/
+    /*
+        Вспомогательные методы
+    */
 
-	public function convert_str_encoding($str, $to_encoding, $from_encoding, $alt = false)
-	{
+    public function convert_str_encoding($str, $to_encoding, $from_encoding, $alt = false)
+    {
+        if (function_exists('iconv')) {
+            $str = @iconv($from_encoding, $to_encoding, $str);
+        } elseif (function_exists('mb_convert_encoding')) {
+            $str = @mb_convert_encoding($str, $to_encoding, $from_encoding);
+        } else {
+            // TODO add сonverting Windows-1251 to UTF-8 and the reverse when no iconv and mb_convert_encoding
+            return $alt ? $alt : $str;
+        }
 
-		if (function_exists('iconv'))
-		{
-			$str = @iconv($from_encoding, $to_encoding, $str);
-		}
-		elseif (function_exists('mb_convert_encoding'))
-		{
-			$str = @mb_convert_encoding($str, $to_encoding, $from_encoding);
-		}
-		else
-		{
-			// TODO add сonverting Windows-1251 to UTF-8 and the reverse when no iconv and mb_convert_encoding
-			return $alt ? $alt : $str;
-		}
-
-		return $str;
-	}
+        return $str;
+    }
 }
